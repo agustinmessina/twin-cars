5ea1d85f108deeb4e82f69120c5da8750bbf0fb0
Messina <agustin.e.messina@accenture.com>
2019-06-28 15:39:34 -0300

    Initial layout of game ended menu

--- src/game/game-component.js
+++ src/game/game-component.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import sketch from './sketch-components/sketch';
 import P5Wrapper from 'react-p5-wrapper';
+import './game-component.css';
 
 class GameComponent extends React.Component {
   render() {
     return (
-      <div>
-        <P5Wrapper sketch={sketch} gameSettings={this.props.gameSettings} />
+      <div className="parent">
+        <div className="container">
+          <h3>Juego terminado!</h3>
+          <hr />
+          <div className="score">
+            <p>Puntaje</p>
+            <p>1300</p>
+            </div><div className="highscore">
+            <p>Puntaje mas alto</p>
+            <p>1500</p>
+          </div>
+          <hr />
+          <div className="buttons">
+            <button>Salir</button>
+            <button>Jugar de nuevo</button>
+          </div>
+        </div>
+        {/* <P5Wrapper sketch={sketch} gameSettings={this.props.gameSettings} /> */}
       </div>
     );
   }
