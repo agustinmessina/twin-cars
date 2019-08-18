a64be3ce2a0e3080881d6f26e318dd142968eef5
Agustin Elian Messina <agustin.e.messina@accenture.com>
2019-08-18 20:58:38 -0300

    Added css to game-ended menu

--- src/game/game-component.js
+++ src/game/game-component.js
@@ -3,10 +3,10 @@ import sketch from './sketch-components/sketch';
 import P5Wrapper from 'react-p5-wrapper';
 import './game-component.css';
 
-function GameComponent() {
+function GameComponent({gameSettings}) {
   return (
-    <div className="parent">
-      <div className="container">
+    <div>
+      {/* <div className="container">
         <h3>Juego terminado!</h3>
         <hr />
         <div className="score">
@@ -18,11 +18,20 @@ function GameComponent() {
         </div>
         <hr />
         <div className="buttons">
-          <button>Salir</button>
+          <button onClick={() => props.onGameFinished()}>Salir</button>
           <button>Jugar de nuevo</button>
         </div>
+      </div> */}
+      <div className="menu menu-game">
+        <h2 className="game-title white-yellow-words">JUEGO TERMINADO!</h2>
+        <div className="scores">
+          <div className="score-text">PUNTAJE</div>
+          <div className="score">15000</div>
+        </div>
+        <button className="secondary-btn quit-btn">SALIR</button>
+        <button className="primary-btn play-again-btn">JUGAR DE NUEVO</button>
       </div>
-      {/* <P5Wrapper sketch={sketch} gameSettings={this.props.gameSettings} /> */}
+      {/* <P5Wrapper sketch={sketch} gameSettings={gameSettings} /> */}
     </div>
   );
 }
